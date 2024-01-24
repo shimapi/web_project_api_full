0e3b0c9ceb1de55d506365ec479c3ff6f39dffd7
Shirley Mallea <85321043+shimapi@users.noreply.github.com>
2024-01-24 16:29:05 -0300

    feat: managing errors in popups

--- frontend/src/components/EditProfilePopup.js
+++ frontend/src/components/EditProfilePopup.js
@@ -1,64 +1,116 @@
 import React, { useState, useContext, useEffect } from "react";
 import PopupWithForm from "./PopupWithForm";
 import { CurrentUserContext } from "../contexts/CurrentUserContext";
+import InfoTooltip from "./InfoTooltip.js";
+import { errEditName, errEditOcupation } from "../utils/variables.js";
 
 export default function EditProfilePopup({ isOpen, onClose, onUpdateUser }) {
 	const currentUser = useContext(CurrentUserContext);
 	const [name, setName] = useState("");
 	const [description, setDescription] = useState("");
+	const [error, setError] = React.useState(false);
+	const [openInfoTool, setOpenInfoTool] = React.useState(false);
+	const [errorMessage, setErrorMessage] = React.useState('errorciiito');
+
+	const methods = {
+		setName,
+		setDescription,
+	}
+	const errorMessages = {
+		editProfileName: errEditName,
+		editProfileAbout: errEditOcupation,
+	}
 
 	useEffect(() => {
 		setName(currentUser.name);
 		setDescription(currentUser.about);
 	}, [currentUser]);
 
+
+	function onInputChange(e) {
+		const fieldName = e.target.name;
+		const dataMethod = e.target.dataset.method;
+		const value = e.target.value;
+
+		//asigno el valor al input correspondiente
+		methods[dataMethod](value); //setPlaceName(e.target.value);
+
+		if (!e.target.validity.valid) {
+			setOpenInfoTool(true)
+			setErrorMessage(errorMessages[fieldName] || 'Error genérico');
+			return setError(true)
+		}
+		setError(false);
+		setErrorMessage('');;
+	}
 	function handleSubmit(e) {
 		e.preventDefault();
-		onUpdateUser({
-			name,
-			about: description,
-		});
+		try {
+			onUpdateUser({
+				name,
+				about: description,
+			});
+		} catch (error) {
+			console.log("Error editando la información", error);
+			setErrorMessage(error)
+			setError(true);
+			setOpenInfoTool(true);
+		}
+	}
+	function handleClose() {
+		setError(false);
+		setOpenInfoTool(false);
 	}
 
-	const handleChangeUsername = (e) => {
-		setName(e.target.value);
-	};
-	const handleChangeDescription = (e) => {
-		setDescription(e.target.value);
-	};
+	/* 	const handleChangeUsername = (e) => {
+			setName(e.target.value);
+		};
+		const handleChangeDescription = (e) => {
+			setDescription(e.target.value);
+		}; */
 
 	return (
-		<PopupWithForm
-			title="Editar Perfil"
-			submitText="Guardar"
-			isOpen={isOpen}
-			onClose={onClose}
-			onSubmit={handleSubmit}
-		>
-			<input
-				type="text"
-				id="edit-profile__name"
-				name="editProfileName"
-				className="form__input modal__input edit-profile__input edit-profile__name"
-				required
-				minLength="2"
-				maxLength="40"
-				value={name || ""}
-				onChange={handleChangeUsername}
+		<>
+			<PopupWithForm
+				title="Editar Perfil"
+				submitText="Guardar"
+				isOpen={isOpen}
+				onClose={onClose}
+				onSubmit={handleSubmit}
+			>
+				<input
+					type="text"
+					id="edit-profile__name"
+					name="editProfileName"
+					data-method='setName'
+					className="form__input modal__input edit-profile__input edit-profile__name"
+					required
+					minLength="2"
+					maxLength="40"
+					value={name || ""}
+					onChange={onInputChange}
+				/>
+				<span className="edit-profile__name-error form__input-error"></span>
+				<input
+					type="text"
+					id="edit-profile__about"
+					name="editProfileAbout"
+					data-method='setDescription'
+					className="form__input modal__input edit-profile__input edit-profile__about"
+					required
+					minLength="2"
+					maxLength="200"
+					value={description || ""}
+					onChange={onInputChange}
+				/>
+				<span className="edit-profile__about-error form__input-error"></span>
+			</PopupWithForm>
+			<InfoTooltip
+				error={error}
+				openInfoTool={openInfoTool}
+				handleClose={handleClose}
+				errorMessage={errorMessage}
 			/>
-			<span className="edit-profile__name-error form__input-error"></span>
-			<input
-				type="text"
-				id="edit-profile__about"
-				name="editProfileAbout"
-				className="form__input modal__input edit-profile__input edit-profile__about"
-				required
-				minLength="2"
-				maxLength="200"
-				value={description || ""}
-				onChange={handleChangeDescription}
-			/>
-			<span className="edit-profile__about-error form__input-error"></span>
-		</PopupWithForm>
+		</>
 	);
 }
--- frontend/src/utils/variables.js
+++ frontend/src/utils/variables.js
@@ -3,8 +3,8 @@
 export const MAIN_URL = "http://localhost:3005";
 
 export const errRegister = 'Hay un problema en tu registro';
-export const errLogin = 'Error en inicio de sesión';
-export const errAddPlaceName = '';
-export const errAddPlaceLink = '';
-export const errEditName = '';
-export const errEditOcupation = '';
+export const errLogin = 'Hay un error en inicio de sesión';
+export const errAddPlaceName = 'Hay un problema con el nombre del lugar';
+export const errAddPlaceLink = 'Hay un problema en el la foto';
+export const errEditName = 'Hay un problema en el nombre';
+export const errEditOcupation = 'Hay un problema en la ocupación';
