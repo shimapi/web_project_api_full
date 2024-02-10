4ebdd8b0f7026704948e4f513120978856e208e9
Shirley Mallea <85321043+shimapi@users.noreply.github.com>
2024-02-09 23:44:58 -0300

    feat: adding close on click outside of popups

--- frontend/src/custom-hooks/useEscKey.js
+++ frontend/src/custom-hooks/useEscKey.js
@@ -2,20 +2,26 @@ import { useEffect } from 'react';
 
 const useEscKey = (handleClose) => {
 
-
   useEffect(() => {
     function handleEscKey(event) {
       if (event.key === 'Escape' || event.key === 'Esc') {
         handleClose();
       }
     }
-
+    function handleClickOutside(event) {
+      if (event.target.classList.contains('modal_active')) {
+        handleClose();
+      }
+    }
     document.addEventListener('keydown', handleEscKey);
+    document.addEventListener('click', handleClickOutside);
 
     return () => {
       document.removeEventListener('keydown', handleEscKey);
+      document.removeEventListener('click', handleClickOutside);
     };
   }, [handleClose]);
+
 };
 
 export default useEscKey;
--- frontend/src/utils/variables.js
+++ frontend/src/utils/variables.js
@@ -1,8 +1,6 @@
 //export const MAIN_URL = "https://api.shirley.lat"; // http://localhost:3000
 export const MAIN_URL = "http://localhost:3005";
 
-//export const errRegister = 'Hay un problema en tu registro';
-//export const errLogin = 'Hay un error en inicio de sesión';
 export const errAddPlaceName = 'Por favor escribe el *nombre* del lugar';
 export const errEditName = 'Por favor escribe tu nombre';
 export const errEditOcupation = 'Por favor escribe tu ocupación';
